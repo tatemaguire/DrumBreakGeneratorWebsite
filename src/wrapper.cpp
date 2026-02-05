dc1aa729a6ed27916411930bb520fd2d87d1169d
Tate Maguire <tatemaguir@gmail.com>
2026-02-04 18:46:23 -0800

    changed wrapper class

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,8 +1,14 @@
 #include <emscripten/bind.h>
+#include <string>
 
 #include "DrumBreakGenerator/src/midi.hpp"
+#include "DrumBreakGenerator/src/generator.hpp"
 
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(my_module) {
+    class_<MIDISequence>("MIDISequence")
+        .function("writeToFile", &MIDISequence::writeToFile)
+        .function("to_string", &MIDISequence::to_string);
+    function("generateSequence", &generateSequence);
 }
