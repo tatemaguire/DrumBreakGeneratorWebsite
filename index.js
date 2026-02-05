dc25ac0cc6e26b583f3329a48f5b92e9187dbb3d
Tate Maguire <tatemaguir@gmail.com>
2026-02-05 14:01:15 -0800

    updated to support more parameters and display output

--- index.js
+++ index.js
@@ -2,34 +2,43 @@ let initialized = false;
 
 var Module = { onRuntimeInitialized: () => { initialized = true; }}
 
-
 const densityInput = document.getElementById("densityInput");
+const subDensityInput = document.getElementById("subDensityInput");
+const chaosInput = document.getElementById("chaosInput");
 const generateButton = document.getElementById("generateButton");
+const outputText = document.getElementById("outputText");
+const downloadLink = document.getElementById("downloadLink");
 
-generateButton.addEventListener("click", () => { generateMIDIFile(densityInput.value); });
+generateButton.addEventListener("click", () => { 
+    generateMIDIFile(densityInput.value, subDensityInput.value, chaosInput.value); 
+});
 
-function generateMIDIFile(density) {
-    density = Number(density);
+function generateMIDIFile(density, subDensity, chaos) {
     if (!initialized) return;
-
+    
+    density = Number(density);
     if (density < 0 || density > 1) {
-        throw new Error("Density is out of range");
+        alert("Density is out of range");
+        return;
+    }
+    subDensity = Number(subDensity);
+    if (subDensity < 0 || subDensity > 1) {
+        alert("Subdivision Density is out of range");
+        return;
+    }
+    chaos = Number(chaos);
+    if (chaos < 0 || chaos > 1) {
+        alert("Chaos is out of range");
+        return;
     }
 
-    console.log(typeof(density));
-
     let seq = Module.generateSequence(density);
+    outputText.innerText = seq.to_string();
     let buffer = Module.writeToBuffer(seq);
     seq.delete();
 
     var blob = new Blob([buffer], { type: 'audio/midi' });
-    var link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'out.mid';
-    link.click();
-
-    // const anchor = document.createElement("a");
-    // anchor.href = "./out.mid";
-    // anchor.download = "out.mid";
-    // anchor.click();
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = 'out.mid';
+    downloadLink.style = "visibility: visible";
 }
