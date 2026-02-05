aaa81ee16a277bc78d39e82c425e7c21350ba70c
Tate Maguire <tatemaguir@gmail.com>
2026-02-04 20:05:13 -0800

    set up test interaction

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let initialized = false;
 
-
+var Module = { onRuntimeInitialized: () => { initialized = true; }}
 
 
 const densityInput = document.getElementById("densityInput");
@@ -8,14 +9,18 @@ const generateButton = document.getElementById("generateButton");
 generateButton.addEventListener("click", () => { generateMIDIFile(densityInput.value); });
 
 function generateMIDIFile(density) {
+    if (!initialized) return;
+
     if (density < 0 || density > 1) {
         throw new Error("Density is out of range");
     }
 
-    
+    let seq = Module.generateSequence(0.5);
+    console.log(seq.to_string());
+    seq.writeToFile("out.mid");
 
     const anchor = document.createElement("a");
-    anchor.href = "./favicon.ico";
-    anchor.download = "icon.ico";
+    anchor.href = "./out.mid";
+    anchor.download = "out.mid";
     anchor.click();
 }
