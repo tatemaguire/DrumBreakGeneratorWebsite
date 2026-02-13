f05b837dd824918daf9c197d09d74ff7d3deb5aa
Tate Maguire <tatemaguir@gmail.com>
2026-02-12 16:59:28 -0800

    parameter inputs for all 4 instruments, and changed web interface

--- index.js
+++ index.js
@@ -3,40 +3,56 @@ let initialized = false;
 var Module = { onRuntimeInitialized: () => { initialized = true; }}
 
 const numStepsInput = document.getElementById("numStepsInput");
-const densityInput = document.getElementById("densityInput");
-const subDensityInput = document.getElementById("subDensityInput");
+
+const kickDensityInput = document.getElementById("kickDensityInput");
+const kickSubDensityInput = document.getElementById("kickSubDensityInput");
+const snareDensityInput = document.getElementById("snareDensityInput");
+const snareSubDensityInput = document.getElementById("snareSubDensityInput");
+const hhDensityInput = document.getElementById("hhDensityInput");
+const hhSubDensityInput = document.getElementById("hhSubDensityInput");
+const ohDensityInput = document.getElementById("ohDensityInput");
+const ohSubDensityInput = document.getElementById("ohSubDensityInput");
+
 const generateButton = document.getElementById("generateButton");
 const outputText = document.getElementById("outputText");
 const downloadLink = document.getElementById("downloadLink");
 
 generateButton.addEventListener("click", () => { 
-    generateMIDIFile(numStepsInput.value, densityInput.value, subDensityInput.value); 
+    generateMIDIFile(); 
 });
 
-function generateMIDIFile(numSteps, density, subDensity) {
+function generateMIDIFile() {
     if (!initialized) return;
+
+    const numSteps = Number(numStepsInput.value);
+    const kickDensity = Number(kickDensityInput.value);
+    const kickSubDensity = Number(kickSubDensityInput.value);
+    const snareDensity = Number(snareDensityInput.value);
+    const snareSubDensity = Number(snareSubDensityInput.value);
+    const hhDensity = Number(hhDensityInput.value);
+    const hhSubDensity = Number(hhSubDensityInput.value);
+    const ohDensity = Number(ohDensityInput.value);
+    const ohSubDensity = Number(ohSubDensityInput.value);
     
-    numSteps = Number(numSteps);
+    let configs = new Module.ConfigList();
+    configs.push_back({instrument: 36, density: kickDensity, sub_density: kickSubDensity});
+    configs.push_back({instrument: 37, density: snareDensity, sub_density: snareSubDensity});
+    configs.push_back({instrument: 38, density: hhDensity, sub_density: hhSubDensity});
+    configs.push_back({instrument: 39, density: ohDensity, sub_density: ohSubDensity});
+
+    for (c of configs) {
+        if (c.density < 0 || c.density > 1 || c.sub_density < 0 || c.sub_density > 1) {
+            alert("density or sub_density value(s) are out of range");
+            return;
+        }
+    }
+
     if (numSteps < 0) {
         alert("Number of Steps is out of range");
         return;
     }
 
-    density = Number(density);
-    if (density < 0 || density > 1) {
-        alert("Density is out of range");
-        return;
-    }
-    subDensity = Number(subDensity);
-    if (subDensity < 0 || subDensity > 1) {
-        alert("Subdivision Density is out of range");
-        return;
-    }
-
     let generator = new Module.DrumBreakGenerator();
-    let configs = new Module.ConfigList();
-    configs.push_back({instrument: 36, density: 1, sub_density: 1});
-
     let seq = generator.generateSequence(numSteps, configs);
 
     outputText.innerText = seq.to_string();
